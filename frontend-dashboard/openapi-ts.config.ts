ca335cbf15589870453f92692b919d17a2d5c3cc
Raúl Solano <rasoberdiel3@gmail.com>
2026-03-25 10:36:11 +0100

    feat: configurated openapi

---
+++ frontend-dashboard/openapi-ts.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+  client: "@hey-api/client-fetch",
+  input: "http://localhost:8080/api/v1/v3/api-docs",
+  output: {
+    path: "src/client",
+  },
+  plugins: [
+    "@hey-api/sdk",
+    "@hey-api/typescript"
+  ],
+});
